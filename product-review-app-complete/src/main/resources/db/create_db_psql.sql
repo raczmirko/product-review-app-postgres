1b33427cd5d41ded745c51435e02bebc3a96db91
Mirko Racz <123543210+raczmirko@users.noreply.github.com>
2025-02-09 10:03:11 +0100

    fix: added missing id column

--- product-review-app-complete/src/main/resources/db/create_db_psql.sql
+++ product-review-app-complete/src/main/resources/db/create_db_psql.sql
@@ -96,11 +96,13 @@ CREATE TABLE product (
 );
 
 CREATE TABLE product_characteristic_value (
+    id SERIAL PRIMARY KEY,
     product int,
     characteristic int,
     "value" varchar(100) NOT NULL,
-    CONSTRAINT pk_product_characteristic_value PRIMARY KEY (product, characteristic),
-    CONSTRAINT fk_product_characteristic_value_article FOREIGN KEY (product) REFERENCES product(id)
+    CONSTRAINT fk_product_characteristic_value_article FOREIGN KEY (product) REFERENCES product(id),
+    CONSTRAINT fk_product_characteristic_value_characteristic FOREIGN KEY (characteristic) REFERENCES characteristic(id),
+    CONSTRAINT uq_product_characteristic_value UNIQUE (product, characteristic)
 );
 
 CREATE TABLE product_image (
