2020c64718fd5f7da4b6e0ff89d98cd971ff1c5d
Mirko Racz <123543210+raczmirko@users.noreply.github.com>
2025-02-17 19:04:21 +0100

    fix: added dto-based pagination

--- src/main/java/hu/okrim/productreviewappcomplete/ProductReviewAppCompleteApplication.java
+++ src/main/java/hu/okrim/productreviewappcomplete/ProductReviewAppCompleteApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @SpringBootApplication
 @EnableJpaAuditing
 public class ProductReviewAppCompleteApplication {
